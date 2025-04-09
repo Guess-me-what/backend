6802145f1da77cf58355b922c829e0086d67c014
최미래 <miraechoi@dgsw.hs.kr>
2025-04-09 15:42:11 +0900

    initial commit

---
+++ src/main/java/com/guessmewhat/backend/GuessMeWhatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guessmewhat.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessMeWhatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessMeWhatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guessmewhat/backend/GuessMeWhatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guessmewhat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessMeWhatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
