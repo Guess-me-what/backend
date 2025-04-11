79cbdffbe3e703d9d97fa300c95b8954b12ac336
최미래 <miraechoi@dgsw.hs.kr>
2025-04-11 10:47:58 +0900

    feature(#2): add quiz create request dto

---
+++ src/main/java/com/guessmewhat/backend/domain/quiz/application/dto/request/QuizCreateRequest.java
@@ -0,0 +1,11 @@
+package com.guessmewhat.backend.domain.quiz.application.dto.request;
+
+import com.guessmewhat.backend.domain.quiz.application.dto.response.QuestionCreateRequest;
+
+import java.util.List;
+
+public record QuizCreateRequest(
+        String nickname, // 유저 닉네임
+        String introduction, // 한 줄 소개
+        List<QuestionCreateRequest> questions // 문제 리스트 (5개)
+) {}
