3da413ddb81df7c83baa252dee93ae5afead96b4
최미래 <miraechoi@dgsw.hs.kr>
2025-04-11 10:47:07 +0900

    feature(#2): add quiz set entity

---
+++ src/main/java/com/guessmewhat/backend/domain/quiz/domain/QuizSet.java
@@ -0,0 +1,43 @@
+package com.guessmewhat.backend.domain.quiz.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuizSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String nickname;
+
+    private String introduction;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expirationDate;
+
+    @OneToMany(mappedBy = "quizSet", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Quiz> quizzes = new ArrayList<>();
+
+    public QuizSet(String code, String nickname, String introduction, List<Quiz> quizzes) {
+        this.code = code;
+        this.nickname = nickname;
+        this.introduction = introduction;
+        this.createdAt = LocalDateTime.now();
+        this.expirationDate = this.createdAt.plusDays(7);
+        this.quizzes = quizzes;
+        this.quizzes.forEach(q -> q.setQuizSet(this));
+    }
+}
