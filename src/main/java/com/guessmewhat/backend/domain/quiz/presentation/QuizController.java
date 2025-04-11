9702d18345a77a72102a1bcaba718d90be71d224
최미래 <miraechoi@dgsw.hs.kr>
2025-04-11 12:05:42 +0900

    feature(#4): add quiz/{code}/submit api

--- src/main/java/com/guessmewhat/backend/domain/quiz/presentation/QuizController.java
+++ src/main/java/com/guessmewhat/backend/domain/quiz/presentation/QuizController.java
@@ -2,7 +2,9 @@ package com.guessmewhat.backend.domain.quiz.presentation;
 
 import com.guessmewhat.backend.domain.quiz.application.QuizService;
 import com.guessmewhat.backend.domain.quiz.application.dto.request.QuizCreateRequest;
+import com.guessmewhat.backend.domain.quiz.application.dto.request.QuizSubmitRequest;
 import com.guessmewhat.backend.domain.quiz.application.dto.response.QuizSetInfoResponse;
+import com.guessmewhat.backend.domain.quiz.application.dto.response.QuizSubmitResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,15 @@ public class QuizController {
     @GetMapping("/{code}")
     public ResponseEntity<QuizSetInfoResponse> getQuizSetInfo(@PathVariable String code) {
         QuizSetInfoResponse response = quizService.getQuizSetInfo(code);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{code}/submit")
+    public ResponseEntity<QuizSubmitResultResponse> submitQuiz(
+            @PathVariable String code,
+            @RequestBody QuizSubmitRequest request
+    ) {
+        QuizSubmitResultResponse response = quizService.submitQuiz(code, request);
         return ResponseEntity.ok(response);
     }
 
