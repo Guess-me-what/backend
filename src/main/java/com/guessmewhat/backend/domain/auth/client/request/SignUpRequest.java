558570c58dbac57462a2f73414d0c683ff4788d0
최미래 <miraechoi@dgsw.hs.kr>
2025-04-16 23:50:26 +0900

    refactor(#1): add validation to sign up request

--- src/main/java/com/guessmewhat/backend/domain/auth/client/request/SignUpRequest.java
+++ src/main/java/com/guessmewhat/backend/domain/auth/client/request/SignUpRequest.java
@@ -2,11 +2,18 @@ package com.guessmewhat.backend.domain.auth.client.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        @Size(min = 1, max = 6)
         String nickname,
+
+        @NotBlank
+        @Size(min = 8)
         String password
 ){}
